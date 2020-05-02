9b8f14ba82cd4064d58065ce1953d4929b1f16e5
Anthony Tesija <antesija@gmail.com>
2020-05-02 13:05:44 -0700

    Remove unused art

--- src/images/index.js
+++ src/images/index.js
@@ -1,6 +1,3 @@
 export const Sketch1 = require('./sketch/sketch1.png');
 export const Sketch2 = require('./sketch/sketch2.png');
 export const Sketch3 = require('./sketch/sketch3.png');
-export const Art1 = require('./art/art1.png');
-export const Art2 = require('./art/art2.png');
-export const Art3 = require('./art/art3.png');
